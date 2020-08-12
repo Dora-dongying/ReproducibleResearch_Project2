7155c511dbe706c17a4be3f55da3b876ceba7d99
Dora-dongying <dora.dongying@gmail.com>
2020-08-12 00:57:53 -0400

    Fixed the "EXP" factor problem in question 2.

--- Storm_DataAnalysis.R
+++ Storm_DataAnalysis.R
@@ -15,8 +15,8 @@ injury_byType <- aggregate(INJURIES ~ EVTYPE, stormData, sum )
 fatal_byType_Sorted <- fatal_byType[order(-fatal_byType$FATALITIES),]
 injury_byType_Sorted <- injury_byType[order(-injury_byType$INJURIES),]
 
-fatal_byType_Sorted[1:10]
-injury_byType_Sorted[1:10]
+fatal_byType_Sorted[1:10,]
+injury_byType_Sorted[1:10,]
 
 par(mfrow = c(1, 2))
 par(mar = c(10, 4, 4, 2), cex = 0.8, cex.main = 1.2, cex.lab = 1.2)
@@ -26,24 +26,35 @@ barplot(injury_byType_Sorted$INJURIES[1:10], names.arg = injury_byType_Sorted$EV
         main = 'Top 10 Weather Events for Injuries', ylab = 'Number of Injuries')
 
 #### Q2: Across the United States, which types of events have the greatest economic consequences?
-propertyDMG_byType <- aggregate(PROPDMG ~ EVTYPE, stormData, sum)
-cropDMG_byType <- aggregate(CROPDMG ~ EVTYPE, stormData, sum)
-totalDMG_byType <- merge(propertyDMG_byType, cropDMG_byType, by = "EVTYPE")
-totalDMG_byType$TOTALDMG <- totalDMG_byType$PROPDMG + totalDMG_byType$CROPDMG
+library(plyr)
 
-propertyDMG_byType_Sorted <- propertyDMG_byType[order(-propertyDMG_byType$PROPDMG),]
-cropDMG_byType_Sorted <- cropDMG_byType[order(-cropDMG_byType$CROPDMG),]
+stormData <- mutate(stormData, PropertyDamage = ifelse(toupper(PROPDMGEXP) =='K', PROPDMG*1000, 
+                                             ifelse(toupper(PROPDMGEXP) =='M', PROPDMG*1000000, 
+                                                    ifelse(toupper(PROPDMGEXP) == 'B', PROPDMG*1000000000, 
+                                                           ifelse(toupper(PROPDMGEXP) == 'H', PROPDMG*100, PROPDMG)))))
+stormData <- mutate(stormData, CropDamage = ifelse(toupper(CROPDMGEXP) =='K', CROPDMG*1000, 
+                                                       ifelse(toupper(CROPDMGEXP) =='M', CROPDMG*1000000, 
+                                                              ifelse(toupper(CROPDMGEXP) == 'B', CROPDMG*1000000000, 
+                                                                     ifelse(toupper(CROPDMGEXP) == 'H', CROPDMG*100, PROPDMG)))))
+
+propertyDMG_byType <- aggregate(PropertyDamage ~ EVTYPE, stormData, sum)
+cropDMG_byType <- aggregate(CropDamage ~ EVTYPE, stormData, sum)
+totalDMG_byType <- merge(propertyDMG_byType, cropDMG_byType, by = "EVTYPE")
+totalDMG_byType$TOTALDMG <- totalDMG_byType$PropertyDamage + totalDMG_byType$CropDamage
+
+propertyDMG_byType_Sorted <- propertyDMG_byType[order(-propertyDMG_byType$PropertyDamage),]
+cropDMG_byType_Sorted <- cropDMG_byType[order(-cropDMG_byType$CropDamage),]
 totalDMG_byType_Sorted <- totalDMG_byType[order(-totalDMG_byType$TOTALDMG),]
 
-propertyDMG_byType_Sorted[1:10]
-cropDMG_byType_Sorted[1:10]
-totalDMG_byType_Sorted[1:10]
+propertyDMG_byType_Sorted[1:10,]
+cropDMG_byType_Sorted[1:10,]
+totalDMG_byType_Sorted[1:10,]
 
 par(mfrow = c(1, 3))
 par(mar = c(10, 4, 4, 2), cex = 0.8, cex.main = 1.2, cex.lab = 1.2)
-barplot(propertyDMG_byType_Sorted$PROPDMG[1:10], names.arg = propertyDMG_byType_Sorted$EVTYPE[1:10], col = 'blue',
+barplot(propertyDMG_byType_Sorted$PropertyDamage[1:10], names.arg = propertyDMG_byType_Sorted$EVTYPE[1:10], col = 'blue',
         main = 'Top 10 Weather Events for property damages', ylab = 'Property Damages')
-barplot(cropDMG_byType_Sorted$CROPDMG[1:10], names.arg = cropDMG_byType_Sorted$EVTYPE[1:10], col = 'green',
+barplot(cropDMG_byType_Sorted$CropDamage[1:10], names.arg = cropDMG_byType_Sorted$EVTYPE[1:10], col = 'green',
         main = 'Top 10 Weather Events for crop damages', ylab = 'Crop Damages')
 barplot(totalDMG_byType_Sorted$TOTALDMG[1:10], names.arg = totalDMG_byType_Sorted$EVTYPE[1:10], col = 'orange',
         main = 'Top 10 Weather Events for total damages', ylab = 'Total Damages')
